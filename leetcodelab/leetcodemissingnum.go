57ed756a05b3083f96b106f602eb2e0bdc3259b2
Silverthg <fmosorkin@gmail.com>
2021-10-30 15:02:01 +0900

    доработал лаб2

--- leetcodelab/leetcodemissingnum.go
+++ leetcodelab/leetcodemissingnum.go
@@ -2,20 +2,45 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"strconv"
 )
 
-func find_missing_number(arr []int, n int) int {
-	sum := 0
-	sum1 := 0
-	for i := 0; i < n; i++ {
-		sum += arr[i] //
-	}
-	sum1 = ((n + 1) * (n + 2)) / 2
-	return (sum1 - sum)
-}
 func main() {
-	arr := []int{5, 6, 8, 1, 2, 9, 4, 3}
-	size1 := len(arr)
-	missing_no := find_missing_number(arr, size1)
-	fmt.Printf("В массиве с размером %d пропущена %d", size1+1, missing_no)
+	http.HandleFunc("/", missing_number_handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func missing_number(arr []int) {
+	sum := 0
+	idx := -1
+	for i := 0; i < len(arr); i++ {
+		if arr[i] == 0 {
+			idx = i
+		} else {
+			sum += arr[i]
+		}
+	}
+
+	total := (len(arr) + 1) * len(arr) / 2
+
+	fmt.Printf("missing number is %d at index %d ", total-sum, idx)
+}
+
+func missing_number_handler(w http.ResponseWriter, r *http.Request) {
+
+	r.ParseForm()
+	missing_number(parr(r.FormValue("key")))
+
+}
+
+func parr(s string) []int {
+	arr := make([]int, len(s)-1)
+	sa, _ := strconv.Atoi(s)
+	for i := 0; i < len(s)-1; i++ {
+		arr[i] = sa % 10
+		sa = sa / 10
+
+	}
+	return arr
 }
